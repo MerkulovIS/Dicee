1aa3d7072ac980cfaedf5972aafa4e2f9b680567
MerkulovIS <imerkulov@me.com>
2019-02-04 17:29:46 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Ilya Merkulov on 04/02/2019.
+//  Copyright © 2019 Ilya Merkulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
