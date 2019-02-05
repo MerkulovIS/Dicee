b285e8e9388328fe95efed3f9cc989698c615914
MerkulovIS <imerkulov@me.com>
2019-02-06 01:19:10 +0300

    Add shake motion detection

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -35,5 +35,9 @@ class ViewController: UIViewController {
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+
 }
 
